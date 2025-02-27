d9e9a2417ba95c58b98604e4beb7e3ba2ecba785
elmorem <elmorem@gmail.com>
2025-02-26 20:18:09 -0800

    46 47 77

---
+++ backtracking/47_permutations_II.py
@@ -0,0 +1,35 @@
+'''
+Given a collection of numbers, `nums`, that might contain duplicates, return _all possible unique permutations **in any order**._
+
+**Example 1:**
+
+**Input:** nums = [1,1,2]
+**Output:**
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+INTUiTION:  we are going to deal with the duplicate numbers in the array by using a Counter object and iterating through that one instad of the simply array 
+'''
+from collections import Counter
+def permuteUnique(nums):
+    ans, sol = [], []
+    count = Counter(nums)
+    def backtrack():
+        if len(sol) == len(nums):
+            ans.append(sol[:])
+            return
+        for n in count:
+            if count[n]>0:
+                sol.append(n)
+                count[n] -=1  # decrement the count
+                backtrack() # recursive call
+                count[n] +=1 # increment the count
+                sol.pop() # then remove it
+    backtrack()
+    return ans
+
+print(permuteUnique([1,1,2])) # [[1,1,2],[1,2,1],[2,1,1]]
+print("-------------")
+print(permuteUnique([1,1,2,2])) # [[1,1,2,2],[1,2,1,2],[1,2,2,1],[2,1,1,2],[2,1,2,1],[2,2,1,1]]
+print("-------------")
+print(permuteUnique([1,1,2,2,3])) # [[1,1,2,2,3],[1,1,2,3,2],[1,1,3,2,2],[1,2,1,2,3],[1,2,1,3,2],[1,2,2,1,3],[1,2,2,3,1],[1,2,
---
+++ backtracking/77_combinations.py
@@ -0,0 +1,29 @@
+'''
+Given two integers `n` and `k`, return _all possible combinations of_ `k` _numbers chosen from the range_ `[1, n]`.
+
+You may return the answer in **any order**.
+'''
+from typing import List
+
+def combine( n: int, k: int) -> List[List[int]]:
+    ans = []
+    sol = []
+    def backtrack(x):
+        if len(sol) == k:
+            ans.append(sol[:])
+            return
+        left = x
+        still_need = k - len(sol)
+        if left > k - len(sol):
+            backtrack(x-1)
+        sol.append(x)
+        backtrack(x-1)
+        sol.pop()
+    backtrack(n)
+    return ans
+
+print(combine(4,2)) # [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+print("-------------")
+print(combine(4,3)) # [[1,2,3],[1,2,4],[1,3,4],[2,3,4]]
+print("-------------")
+print(combine(4,4)) # [[1,2,3,4]]
