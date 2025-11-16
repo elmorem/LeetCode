8ba910a2a5c4d7290686013da560d9a0d8a5a670
elmorem <elmorem@gmail.com>
2025-11-16 14:05:20 -0800

    2

---
+++ assorted/2.py
@@ -0,0 +1,49 @@
+'''
+write a function that, given a string S contains an HTML fragment
+- all HTHML tages except (a) and (ing> are completely removed
+(a href="url"> tage is replaces with (link:url), where
+url is copeid from te orgiinal tage
+(img src = 'url"> tage is replaced with image:url) where url is copied from the original tage
+in the text all symbols except english letters and digits are removed (=except in the URLS from (a> and <img> tages
+words are converted to lower case
+all words separated by one space))
+
+'''
+from html.parser import HTMLParser
+import re
+
+def solution(S):
+    class Cleaner(HTMLParser):
+        def __init__(self):
+            super().__init__()
+            self.out = []
+        def handle_starttag( self, tag, attrs):
+            attrs = dict(attrs)
+            if tag == "a":
+                href = attrs.get("href")
+                if href:
+                    self.out.append(f"(link:{href})")
+            elif tag == "img":
+                src = attrs.get("src")
+                if src:
+                    self.out.append(f"(image:{src})")
+                    
+        def handle_data(self, data):
+            words = re.findall(r"[A-Za-z0-9]+", data)
+            self.out.extend(w.lower() for w in words)
+
+    parser = Cleaner()
+    parser.feed(S)
+    parser.close()
+    
+    return " ".join(parser.out)
+if __name__ == "__main__":
+    S = '<div>Hello <a href="http://example.com">Example</a>! Here is an image: <img src="http://example.com/image.png"></div>'
+    print(solution(S))  # Expected output: "hello (link:http://example.com) example here is an image (image:http://example.com/image.png)"
+    S = '<p>Check this out: <a href="https://openai.com">OpenAI</a> and this image <img src="https://openai.com/logo.png"></p>'
+    print(solution(S))  # Expected output: "check this out (link:https://openai.com) openai and this image (image:https://openai.com/logo.png)"
+
+    s1 = "<p>hello, ... WORLD!</p>"
+    print(solution(s1))
+    s2 = "<a href='toptal.com'>Toptal_URL</a>now<img src='logo.png'>end"
+    print(solution(s2))  # Expected: "(link:toptal.com) toptal url now (image:logo.png) end"
